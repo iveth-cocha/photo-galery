35c1d4365217b37db2a1b4481948c81a2b78b801
iveth-cocha <maria.iveth16@outlook.es>
2024-05-10 14:55:57 -0500

    guardado en fylesistem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -11,6 +11,42 @@ export class PhotoService {
   public photos: UserPhoto[]=[];
 
   constructor() { }
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+  
+    // Write the file to the data directory
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 
   public async addNewToGallery(){
     //Take a photo
@@ -22,10 +58,12 @@ export class PhotoService {
 
     });
 
-    this.photos.unshift({
+    /*this.photos.unshift({
       filepath:"soon...",
       webviewPath: capturedPhoto.webPath!
-    }); 
+    }); */
+  const savedImageFile = await this.savePicture(capturedPhoto);
+  this.photos.unshift(savedImageFile);
 }
 }
 export interface UserPhoto{
